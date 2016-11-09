6161a0c15e056f4f2ce79e121fae66bb680351d6
Caleb Faught <faughtcj@gmail.com>
2016-11-08 18:37:57 -0600

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -10,35 +10,51 @@ var artists = Object.keys(jukeboxSongs);
 var theArtist = artists[Math.floor(Math.random()) * artists.length];
 var songToPlay = jukeboxSongs[theArtist];
 
+/////////////////////
+// Named Functions //
+/////////////////////
+
+function menuHandler(e) {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
+
+function playHandler(e) {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
+}
+
+function pauseHandler(e) {
+  $('#songPaused').html(songToPlay + " is paused");
+}
+
+function showHandler(e) {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
+}
+
+//////////////////////////////
+// Where functions are used //
+//////////////////////////////
+
 function randomNumber() {
   return Math.floor(Math.random());
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', menuHandler);
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', playHandler);
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', pauseHandler);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', showHandler);
 }
